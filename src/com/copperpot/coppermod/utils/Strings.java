34943cf74edcf71ecf27fcd2e1db5a681fa4ccfc
Jamie <jamie@jeraff.com>
2013-12-29 13:41:09 -0800

    fix ratio

--- src/com/copperpot/coppermod/utils/Strings.java
+++ src/com/copperpot/coppermod/utils/Strings.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public abstract class Strings {
     public static String FART_EVERYONE = "%s just %s on everyone!";
     public static String FART_ONE = "%s just %s on %s!";
-    public static String SCORE_PLAYER = "%s has %s kills, %s deaths [%s] \n";
+    public static String SCORE_PLAYER = "%s has %s kills, %s deaths [%.2f] \n";
     public static String ERR_NO_PLAYER = "No such player.";
     public static List<String> FART_TERMS = Arrays.asList(
             "tooted", "farted", "ripped one", "cut the cheese", "passed gas", "broke wind"
